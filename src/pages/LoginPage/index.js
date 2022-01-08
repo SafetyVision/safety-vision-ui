1896c6ec44649944d483b73a42b24227a60e9a36
Lewis Rafuse <lewisrafuse@gmail.com>
2022-01-08 12:15:11 -0500

    Fixing some routing bugs.

--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom';
 
 export default function LoginPage({ authInfo }) {
-  if (authInfo.isAuthenticated || !authInfo.isExistingUser) {
+  if (authInfo.isAuthenticated) {
     return <Navigate to="/" />;
   }
 
--- src/pages/SignUpPage/index.js
+++ src/pages/SignUpPage/index.js
@@ -1,9 +1,3 @@
-import { Navigate } from 'react-router-dom';
-
 export default function SignUpPage({ authInfo }) {
-  if (authInfo.isAuthenticated || !authInfo.isExistingUser) {
-    return <Navigate to="/" />;
-  }
-
   return <p>SignUp Page</p>;
 }
