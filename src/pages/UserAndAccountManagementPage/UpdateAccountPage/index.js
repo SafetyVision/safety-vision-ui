913bf6573f9e13289555b5c87505132417195bdd
Lewis Rafuse <lewisrafuse@gmail.com>
2022-01-09 22:21:29 -0500

    Fix up the edit account page.

--- src/pages/UserAndAccountManagementPage/UpdateAccountPage/index.js
+++ src/pages/UserAndAccountManagementPage/UpdateAccountPage/index.js
@@ -3,13 +3,26 @@ import axios from 'util/axiosConfig';
 import { Button, Form, FormGroup, Label, Input, Toast, ToastBody, ToastHeader } from 'reactstrap';
 
 export default function UpdateAccountPage({ authInfo }) {
+  const successToastContent = {
+    header: 'Account name updated',
+    body: 'Your account Name was successfully updated.',
+  };
+
+  const errorToastContent = {
+    header: 'Account name could not be updated',
+    body: 'Try again with a valid account name.',
+  }
+
   const [accountName, setAccountName] = useState('');
   const [isOpen, setIsOpen] = useState(false);
+  const [originalAccountName, setOriginalAccountName] = useState('');
+  const [toastContent, setToastContent] = useState(successToastContent);
 
   useEffect(() => {
     if (authInfo.currentUser) {
       axios.get(`/api/accounts/${authInfo.currentUser.account}`).then((res) => {
         setAccountName(res.data.account_name);
+        setOriginalAccountName(res.data.account_name);
       });
     }
   }, [authInfo.currentUser]);
@@ -19,22 +32,28 @@ export default function UpdateAccountPage({ authInfo }) {
       account_name: accountName.trim(),
       login_identifier: accountName.toLowerCase(),
     }).then(() => {
+      setToastContent(successToastContent);
+      setIsOpen(true);
+      setOriginalAccountName(accountName);
+    }).catch(() => {
+      setAccountName(originalAccountName);
+      setToastContent(errorToastContent);
       setIsOpen(true);
     });
   };
 
   return (
     <div>
-      <h1 className="pb-4 fw-bold">Update Your Account Name</h1>
-      <Toast isOpen={isOpen} className="w-100 mb-3">
-        <ToastHeader toggle={() => setIsOpen(false)}>
-          Account name updated
-        </ToastHeader>
-        <ToastBody>
-          Your account Name was successfully updated.
-        </ToastBody>
-      </Toast>
-      <Form>
+      <h1 className="pb-4 fw-bold">Edit Account</h1>
+      <Form style={{ maxWidth: '500px' }} className="mx-auto">
+        <Toast isOpen={isOpen} className="w-100 mb-3">
+          <ToastHeader toggle={() => setIsOpen(false)}>
+            {toastContent.header}
+          </ToastHeader>
+          <ToastBody>
+            {toastContent.body}
+          </ToastBody>
+        </Toast>
         <FormGroup>
           <Label>
             Company/Organization Name
@@ -45,7 +64,7 @@ export default function UpdateAccountPage({ authInfo }) {
             style={{ maxWidth: '500px' }}
           />
         </FormGroup>
-        <Button color="primary" onClick={updateAccount}>Update Account Name</Button>
+        <Button color="primary" onClick={updateAccount} className="w-100">Update Account Name</Button>
       </Form>
     </div>
   );
