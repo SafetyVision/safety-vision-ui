82ef25b198ee26f1801cdf0ab8305d8b01a30ef1
Lewis Rafuse <lewisrafuse@gmail.com>
2022-02-07 22:09:00 -0500

    Adds the rest of login page test.

--- src/pages/LoginPage/LoginPage.test.js
+++ src/pages/LoginPage/LoginPage.test.js
@@ -1,27 +1,54 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import LoginPage from './index';
+import renderWithRouter from 'testing/util';
 
-const authInfo = {
+const authInfoNotAuthenticated = {
   isAuthenticated: false,
   isExistingUser: false,
   currentUser: null,
 };
 
-const setIsAuthenticated = () => {};
+const authInfoAuthenticated = {
+  isAuthenticated: true,
+  isExistingUser: true,
+  currentUser: {
+    email: "test@email.com",
+  },
+}
+
+const setIsAuthenticated = jest.fn();
 
 describe('Login Page', () => {
   it('should render the Login Page with all form fields', async () => {
-    render(<LoginPage authInfo={authInfo} setIsAuthenticated={setIsAuthenticated} />);
+    render(<LoginPage authInfo={authInfoNotAuthenticated} setIsAuthenticated={setIsAuthenticated} />);
     expect(await screen.findByLabelText('Email')).toBeInTheDocument();
     expect(await screen.findByLabelText('Password')).toBeInTheDocument();
     expect(await screen.findByRole('button', { name: 'Log In' })).toBeInTheDocument();
   });
+
   it('should allow the user to input a valid email and password', async () => {
-    render(<LoginPage authInfo={authInfo} setIsAuthenticated={setIsAuthenticated} />);
+    render(<LoginPage authInfo={authInfoNotAuthenticated} setIsAuthenticated={setIsAuthenticated} />);
     userEvent.type(await screen.findByLabelText('Email'), 'test@email.com');
     userEvent.type(await screen.findByLabelText('Password'), 'validpassword123');
     userEvent.click(await screen.findByRole('button', { name: 'Log In' }));
-    expect(true).toBe(true);
+    await waitFor(() => expect(setIsAuthenticated).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(setIsAuthenticated).toHaveBeenCalledWith(true));
+  });
+
+  it('should render an error message when API returns an error', async () => {
+    render(<LoginPage authInfo={authInfoNotAuthenticated} setIsAuthenticated={setIsAuthenticated} />);
+    userEvent.type(await screen.findByLabelText('Email'), 'test@email.com');
+    userEvent.type(await screen.findByLabelText('Password'), 'invalidpassword123');
+    userEvent.click(await screen.findByRole('button', { name: 'Log In' }));
+    expect(await screen.findByText('Log In Failed')).toBeInTheDocument();
+  });
+
+  it('should produce Dashboard Page redirect when the user is authenticated', async () => {
+    renderWithRouter(
+      <LoginPage authInfo={authInfoAuthenticated} setIsAuthenticated={setIsAuthenticated} />,
+      { route: '/login' }
+    );
+    expect(await screen.findByTestId('location-display')).toHaveTextContent('Current location: /');
   });
 });
--- src/testing/util.js
+++ src/testing/util.js
@@ -1,10 +1,24 @@
+import { createMemoryHistory } from 'history';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+
+const LocationDisplay = () => {
+  const location = useLocation()
+
+  return <div data-testid="location-display">{`Current location: ${location.pathname}`}</div>
+}
 
 const renderWithRouter = (ui, {route = '/'} = {}) => {
-  window.history.pushState({}, 'Test page', route)
-
-  return render(ui, {wrapper: BrowserRouter})
-}
+  const history = createMemoryHistory();
+  history.push('/login');
+  return render(
+    <BrowserRouter history={history}>
+      <Routes>
+        <Route path={route} element={ui} />
+        <Route path="*" element={<LocationDisplay />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default renderWithRouter;
