a4e06813799107b1cf334920a73ac37a15ba483c
Lewis Rafuse <lewisrafuse@gmail.com>
2022-02-26 12:03:24 -0500

    Adds back button text prop.

--- src/components/BackButton/index.js
+++ src/components/BackButton/index.js
@@ -2,12 +2,12 @@ import { ArrowLeftCircleFill } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap';
 
-export default function BackButton({ to }) {
+export default function BackButton({ to, text }) {
   return (
     <Button tag={Link} to={to} className="d-flex justify-content-between align-items-center mb-4 px-3" style={{ width: '100px' }}>
       <ArrowLeftCircleFill />
       <span>
-        Back
+        {text}
       </span>
     </Button>
   );
