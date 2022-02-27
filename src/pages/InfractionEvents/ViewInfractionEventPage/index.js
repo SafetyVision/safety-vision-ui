16053e2fd16181aae17f8ad48aaa98bd0d19f823
Lewis <47062950+rafusel@users.noreply.github.com>
2022-02-27 11:41:40 -0500

    Adds location and infraction type to infraction events pages (#23)
    * Adds location and infraction type to events pages.
    
    * Adds better error checking.
    
    * More error checking.
    
    * Set is loaded to false before api call.

--- src/pages/InfractionEvents/ViewInfractionEventPage/index.js
+++ src/pages/InfractionEvents/ViewInfractionEventPage/index.js
@@ -1,19 +1,35 @@
 import { Fragment, useEffect, useState } from 'react';
 import axios from 'util/axiosConfig';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { Spinner } from 'reactstrap';
 import BackButton from 'components/BackButton';
 import formatTimestamp from 'util/dates';
+import ResourceNotFoundPage from 'pages/ErrorPages/ResourceNotFoundPage';
 
 export default function ViewInfractionEvent() {
   const params = useParams();
   const [infractionEvent, setInfractionEvent] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
+    setIsLoaded(false);
     axios.get(`/api/infraction_events/${params.infractionEventId}`).then((res) => {
       setInfractionEvent(res.data);
+      setIsLoaded(true);
+      setIsError(false);
+    }).catch(() => {
+      setIsError(true);
     });
   }, [params.infractionEventId]);
+
+  if (isError) {
+    return <ResourceNotFoundPage />;
+  }
+
+  if (!isLoaded) {
+    return <Spinner />;
+  }
 
   return (
     <div>
@@ -27,9 +43,21 @@ export default function ViewInfractionEvent() {
             <label className="fw-bold pt-3">
               Infraction Time
             </label>
-            <p>
+            <p className="mb-0">
               {formatTimestamp(infractionEvent.infraction_date_time)}
             </p>
+            <label className="fw-bold pt-3">
+              Location
+            </label>
+            <Link to={`/locations/${infractionEvent.infraction_type.id}/view`} className="d-block text-decoration-none">
+              {infractionEvent.infraction_type.infraction_type_name}
+            </Link>
+            <label className="fw-bold pt-3">
+              Infraction Type
+            </label>
+            <Link to={`/infraction-types/${infractionEvent.location.id}/view`} className="d-block text-decoration-none mb-5">
+              {infractionEvent.location.description}
+            </Link>
             <div className="w-100 d-flex justify-content-center">
               <video controls key={infractionEvent.id} className="w-100" style={{ maxWidth: '1000px' }}>
                 <source src={infractionEvent.infraction_video.file} type="video/mp4" />
@@ -37,7 +65,7 @@ export default function ViewInfractionEvent() {
             </div>
           </Fragment>
         ) : (
-          <Spinner />
+          <p>Something went wrong.</p>
         )
       }
     </div>
