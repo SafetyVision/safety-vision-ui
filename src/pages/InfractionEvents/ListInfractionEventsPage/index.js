c11fc6c7830a3ed531ed3a516e0ce28850675fd2
Lewis Rafuse <lewisrafuse@gmail.com>
2022-03-24 05:51:19 -0400

    Adds location and infraction types filters

--- src/pages/InfractionEvents/ListInfractionEventsPage/index.js
+++ src/pages/InfractionEvents/ListInfractionEventsPage/index.js
@@ -1,25 +1,69 @@
 import  { useState, useEffect } from 'react';
 import axios from 'util/axiosConfig';
-import { Table, Spinner, Button } from 'reactstrap';
+import { Table, Spinner, Button, Input, Label, FormGroup } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import formatTimestamp from 'util/dates';
 import ResourceNotFoundPage from 'pages/ErrorPages/ResourceNotFoundPage';
 
+const FILTER_OPTIONS = {
+  location: {
+    filterKey: 'location',
+    displayName: 'Location',
+    displayNameKey: 'description',
+  },
+  infractionType: {
+    filterKey: 'infraction_type',
+    displayName: 'Infraction Type',
+    displayNameKey: 'infraction_type_name',
+  },
+  noFilter: {
+    filterKey: null,
+    displayName: 'No Filter',
+    displayNameKey: null,
+  },
+}
+
 export default function ListInfractionEventsPage() {
-  const [infractionEvents, setInfractionEvents] = useState(null)
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [infractionEvents, setInfractionEvents] = useState(null);
+  const [allInfractionEvents, setAllInfractionEvents] = useState(null);
   const [isError, setIsError] = useState(false);
+  const [filter, setFilter] = useState("noFilter");
+  const [selectedFilterValue, setSelectedFilterValue] = useState("none");
+  const [possibleFilterValues, setPossibleFilterValues] = useState({});
 
   useEffect(() => {
-    setIsLoaded(false);
     axios.get('/api/infraction_events/').then((res) => {
-      setInfractionEvents(res.data);
-      setIsLoaded(true);
-      setIsError(false);
+      setAllInfractionEvents(res.data);
+    }).catch(() => {
+      setIsError(true);
+    });
+    axios.get('/api/locations/').then((res) => {
+      setPossibleFilterValues(possibleFilterValues => ({
+        ...possibleFilterValues,
+        [FILTER_OPTIONS.location.filterKey]: res.data,
+      }));
+    }).catch(() => {
+      setIsError(true);
+    });
+    axios.get('/api/infraction_types/').then((res) => {
+      setPossibleFilterValues(possibleFilterValues => ({
+        ...possibleFilterValues,
+        [FILTER_OPTIONS.infractionType.filterKey]: res.data,
+      }));
     }).catch(() => {
       setIsError(true);
     });
   }, []);
+
+  useEffect(() => {
+    const filterObject = FILTER_OPTIONS[filter];
+    if (!filterObject.filterKey || selectedFilterValue === "none") setInfractionEvents(allInfractionEvents);
+    else {
+      setInfractionEvents(
+        allInfractionEvents.filter(event => `${event[filterObject.filterKey].id}` === selectedFilterValue)
+      );
+    }
+  }, [selectedFilterValue, filter, allInfractionEvents])
 
   const mapInfractionEventToTableRow = (infractionEvent) => (
     <tr key={infractionEvent.id}>
@@ -47,11 +91,20 @@ export default function ListInfractionEventsPage() {
     </tr>
   );
 
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+    setSelectedFilterValue("none");
+  }
+
   if (isError) {
     return <ResourceNotFoundPage />;
   }
 
-  if (!isLoaded) {
+  if (
+    !infractionEvents ||
+    !possibleFilterValues[FILTER_OPTIONS.location.filterKey] ||
+    !possibleFilterValues[FILTER_OPTIONS.infractionType.filterKey]
+  ) {
     return <Spinner />;
   }
 
@@ -60,6 +113,54 @@ export default function ListInfractionEventsPage() {
       <h1 className="fw-bold">
         Infraction Events
       </h1>
+      <FormGroup>
+        <Label>Filter By:</Label>
+        <Input
+          name="filter-selector"
+          id="filter-selector"
+          type="select"
+          value={filter}
+          onChange={handleFilterChange}
+        >
+          <option value="noFilter">
+            {FILTER_OPTIONS.noFilter.displayName}
+          </option>
+          <option value="location">
+            {FILTER_OPTIONS.location.displayName}
+          </option>
+          <option value="infractionType">
+            {FILTER_OPTIONS.infractionType.displayName}
+          </option>
+        </Input>
+      </FormGroup>
+      {
+        FILTER_OPTIONS[filter].filterKey && (
+          <FormGroup>
+            <Label>{`Select ${FILTER_OPTIONS[filter].displayName}:`}</Label>
+            <Input
+              type="select"
+              value={selectedFilterValue}
+              onChange={e => setSelectedFilterValue(e.target.value)}
+            >
+              <option
+                value="none"
+              >
+                {`Select ${FILTER_OPTIONS[filter].displayName}...`}
+              </option>
+              {
+                possibleFilterValues[FILTER_OPTIONS[filter].filterKey].map(filterValue => (
+                  <option
+                    value={filterValue.id}
+                    key={`${filter}${filterValue.id}`}
+                  >
+                    {filterValue[FILTER_OPTIONS[filter].displayNameKey]}
+                  </option>
+                ))
+              }
+            </Input>
+          </FormGroup>
+        )
+      }
       {
         infractionEvents?.length ? (
           <Table striped borderless responsive>
